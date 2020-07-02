99b40e7239d3cd531cae2bfb0ad0be2d49136c1b
diegosoriarios <diegosoriarios@gmail.com>
2020-07-02 18:26:05 -0300

    first commit

---
+++ EmojiArt/ContentView.swift
@@ -0,0 +1,16 @@
+//
+//  ContentView.swift
+//  EmojiArt
+//
+//  Created by diego.rios on 02/07/20.
+//  Copyright © 2020 diego.rios. All rights reserved.
+//
+
+import SwiftUI
+
+struct EmojiArtDocumentView: View {
+    @ObservedObject var document: EmojiArtDocument
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
